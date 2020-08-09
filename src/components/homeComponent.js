0935b57cde8223f9be418fd73daca0e09aaca735
dayesouza <day.carol18@gmail.com>
2020-08-09 19:53:56 -0300

    test hello

--- src/components/homeComponent.js
+++ src/components/homeComponent.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function HomeComponent(props) {
-  return <div className="white-board">Hello Day!</div>;
+  return (
+    <div className="white-board">
+      <h1>Hello Day!</h1>
+    </div>
+  );
 }
